e3d09f2a134d0a91818ac987685a4bb2a7122b94
7abelgebru <7abelgebru@gmail.com>
2024-06-14 03:42:03 -0900

    Day 2

--- Day_2_tip-calculator.py
+++ Day_2_tip-calculator.py
@@ -1,14 +1,11 @@
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
-people = int(input("How many people to split the bill?"))
+print('welcom to the tip calculator!')
+total_bill = float(input('What was the total bill? $'))
+total_num_of_people = int(input('how many people to split the bill? '))
+tip = float(input('How much tip would you like to give 10, 12, or 15? '))
+tip_in_percent = total_bill * tip/100
+bill_to_pay = total_bill + tip_in_percent
+bill_amount = bill_to_pay/total_num_of_people
 
-tip_as_percent = tip / 100
-total_tip_amount = bill * tip_as_percent
-total_bill = bill + total_tip_amount
-bill_per_person = total_bill / people
-final_amount = round(bill_per_person, 2)
+final_bill_to_pay = "{:.2f}".format(bill_amount)
 
-
-
-print(f"Each person should pay: ${final_amount}")
+print(f'Each person should pay: ${final_bill_to_pay}')
---
+++ Day_7_Hangman.py
@@ -0,0 +1,89 @@
+import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+live = 6
+dispaly = []
+for i in range(len(chosen_word)):
+    dispaly += "_"
+while "_" in dispaly:
+    guess = input("Guess a letter: ").lower()
+    
+
+    for i in range(len(chosen_word)): 
+        if chosen_word[i] == guess:
+            dispaly[i] = guess
+    
+    print(dispaly)
+    if guess not in chosen_word:
+        live -= 1
+        if live == 0:
+            break
+            print('you lose')
+    else:
+        print('you won!')
+
+
+
