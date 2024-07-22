0e7a2ccb4360ff3895d20f94a4010d73691e89ca
7abelgebru <7abelgebru@gmail.com>
2024-07-22 02:51:46 -0900

    Day 7

--- Day_9_blind-auction.py
+++ Day_9_blind-auction.py
@@ -1,31 +1,27 @@
-import blind_acution_art
-import os
-import sys
-print(blind_acution_art.logo)
-print("Welcome to the secret auction program.")
-auction = {}
+from blind_acution_art import logo
+from os import system
+print(logo)
+bidder = {}
+def bidder_game(user_name, user_bid):
+        bidder[user_name] = user_bid
+end_of_bid = True
+while  end_of_bid:
+    name = input('What is your name?: ')
+    bid = int(input('What is your bid?: '))
+    bidder_game(user_name=name, user_bid=bid)
+    new_bidder = input("Are there any other bidder? Type 'yes' or 'no'.: ")
+    system('cls')
+    if new_bidder == 'no':
+        end_of_bid = False
+val = 0
+for item in bidder:
+    x = bidder[item]
+    if x > val:
+        val = x
+print(f'The winner is {name} with a bid of ${bid}.')
 
-def finished(b_auction):
-    highest_value = 0 
-    for value in auction:
-        a = auction[value]
-        if a > highest_value:
-            highest_value = a
-            print(f"The winner is {value} with a bid of ${highest_value}.")
-    
-    
-end_of_game = True
-while end_of_game:
-    name = input("What is your name?: ")
-    bid = int(input("What's your bid?: $"))
-    auction[name] =bid
-    bidders = input("Are there any other bidders? 'yes' or 'no'.\n")
-    if bidders == "no":
-        end_of_game = False
-        finished(auction)
-    elif bidders == "yes":
-        os.system('cls')
-    else:
-        print("You typed the incorrect word. Run the code once more while carefully reading the instructions.")
-        sys.exit()
 
+
+
+
+    
